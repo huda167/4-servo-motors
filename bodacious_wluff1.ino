1fa158b5f6cd622c2ba47d113c7b9ab3f7ef1c0b
Huda albaqami <hudalbaqami@gmail.com>
2025-08-02 17:15:47 +0300

    Add files via upload

---
+++ bodacious_wluff1.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+unsigned long startTime;
+
+void setup()
+{
+  servo1.attach(6);
+  servo2.attach(5);
+  servo3.attach(4);
+  servo4.attach(3);
+ 
+  startTime = millis();
+}
+
+void loop()
+{
+   if (millis() - startTime < 2000) {
+    for (int pos = 0; pos <= 180; pos++) {
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      servo4.write(pos);
+      delay(15);
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      servo4.write(pos);
+      delay(15);
+    }
+  }
+  else {
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+  }
+   }
